e5dd3a298ac436b011e96888d19caa55f767696d
Ritik Kumar <143861433+a-b-c-d-ritik@users.noreply.github.com>
2025-06-02 18:06:19 +0530

    02/05/25

--- 18.Flask/flask/app.py
+++ 18.Flask/flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask;
+from flask import Flask,render_template,request;
 
 app=Flask(__name__)
 
@@ -14,6 +14,20 @@ def welcome2():
 def welcome3():
     return "Kya munna bahar nikalne ka mann h"
 
+@app.route("/p1")
+def welcome4():
+    return "<html><h1> page 1 </h1></html>"
+
+@app.route("/p2",methods=['GET'])
+def welcome5():
+    return render_template("index.html")
+
+@app.route("/form",methods=['GET','POST'])
+def mfrom():
+    if request.method=="POST":
+        name=request.form['name']
+        return (f' Hello {name} !!')
+    return render_template("form.html")
 
 if __name__=="__main__":
     app.run(debug=True)
